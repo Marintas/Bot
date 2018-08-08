2e0ca456c28dcf419a75644664c62649d6e5cd6f
Marintas <42211392+Marintas@users.noreply.github.com>
2018-08-08 22:34:22 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -3,11 +3,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
     if (message.content === "да"){
-    message.reply("нет");
-}
-if (message.content=="нет"){
-    message.reply("да");
-}
+      message.reply("нет");
+             }
+    if (message.content=="нет"){
+      message.reply("да");
+             }
     if(message.content == "sh!repeat"){
       setInterval(function() {
           let answers = ["иринагей", "ирина тупая", "ирина сосет", "ирина фрик"]; 
@@ -15,10 +15,9 @@ if (message.content=="нет"){
         message.channel.send(answers[rand]);
       setTimeout(function() {
         clearInterval(any);
-      }, 50000);
-    }, 10000) 
-         }
+             }, 50000);
+               }, 10000) 
+                  }
 });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
