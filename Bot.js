5d239df440dd00d654939a9231be6e4dffd88d6f
Marintas <42211392+Marintas@users.noreply.github.com>
2018-08-08 22:40:17 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,23 +1,16 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-    if (message.content === "да"){
-      message.reply("нет");
-             }
-    if (message.content=="нет"){
-      message.reply("да");
-             }
-    if(message.content == "sh!repeat"){
-      setInterval(function() {
-          let answers = ["иринагей", "ирина тупая", "ирина сосет", "ирина фрик"]; 
-          let rand = Math.floor(Math.random()*answers.length); 
-        message.channel.send(answers[rand]);
-      setTimeout(function() {
-        clearInterval(any);
-             }, 50000);
-               }, 10000) 
-                  }
+client.on('ready', () => {
+    console.log('I am ready!');
 });
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
+
