26859e1e47aa2a6ab3e84bef4056da176d8212de
Marintas <42211392+Marintas@users.noreply.github.com>
2018-08-09 16:32:58 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -24,56 +24,56 @@ client.on('message', message => {
     if (message.content === 'я не тупая') {
     	     message.reply('нет ты тупая');
    	              };
-   if (message.content === 'Да'){
-    message.reply('нет');
+    if (message.content === 'Да'){
+             message.reply('нет');
           };
-   if (message.content === 'Дыа'){
-    message.reply('нет');
-          };
-   if (message.content === 'дыа'){
-    message.reply('нет');
-          };
-   if (message.content === 'нет'){
-    message.reply('да');
-          };
-   if (message.content === 'Нет'){
-      message.reply('да');
+    if (message.content === 'Дыа'){
+            message.reply('нет');
+                  };
+    if (message.content === 'дыа'){
+          message.reply('нет');
+                    };
+    if (message.content === 'нет'){
+            message.reply('да');
+                   };
+    if (message.content === 'Нет'){
+          message.reply('да');
+               };
+    if (message.content === 'Неа'){
+          message.reply('ага');
+               };
+     if (message.content === 'ты тупой'){
+            message.reply('нет');
+                 };
+     if (message.content === 'нет ты'){
+             message.reply('нет');
+             };
+     if (message.content === 'Ты тупой'){
+           message.reply('нет ты');
+                 };
+     if (message.content === 'Соси'){
+          message.reply('ирина, сама соси');
+             };
+     if (message.content === 'Отсосали'){
+          message.reply('пиздос тупая');
+             };
+     if (message.content === 'соси'){
+           message.reply('ирина, сама соси');
+             };
+     if (message.content === 'отсосали'){
+           message.reply('пиздос тупая');
             };
-   if (message.content === 'Неа'){
-    message.reply('ага');
+     if  (message.content === 'ты хуесос'){
+            message.reply('хуи тут сосешь только ты');
+                };
+     if (message.content === 'это факт'){
+           message.reply('это не факт');
+            };
+     if (message.content === 'Это факт'){
+          message.reply('это не факт');
           };
-  if (message.content === 'ты тупой'){
-    message.reply('нет');
-          };
-  if (message.content === 'нет ты'){
-    message.reply('нет');
-          };
-  if (message.content === 'Ты тупой'){
-    message.reply('нет ты');
-          };
-  if (message.content === 'Соси'){
-    message.reply('ирина, сама соси');
-          };
-  if (message.content === 'Отсосали'){
-    message.reply('пиздос тупая');
-          };
-  if (message.content === 'соси'){
-    message.reply('ирина, сама соси');
-           };
-  if (message.content === 'отсосали'){
-    message.reply('пиздос тупая');
-          };
-  if (message.content === 'ты хуесос'){
-    message.reply('хуи тут сосешь только ты');
-          };
-  if (message.content === 'это факт'){
-    message.reply('это не факт');
-          };
-  if (message.content === 'Это факт'){
-    message.reply('это не факт');
-          };
-  if (message.content === 'Ты хуесос'){
-    message.reply('хуи тут сосешь только ты');
+     if (message.content === 'Ты хуесос'){
+          message.reply('хуи тут сосешь только ты');
           };
 });
 
