6088973cc3129caf389e64c57f60563f4547c77a
Marintas <42211392+Marintas@users.noreply.github.com>
2018-08-09 15:34:10 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -10,25 +10,23 @@ client.on('message', message => {
   if (message.content === 'да'){
     message.reply('нет');
           };
-      });
-
- client.on('message', message => {
-     if (message.content === 'я не тупая') {
-    	message.reply('нет ты тупая');
-   	};
-});
- client.on('message', message => {
-     if(message.content === "#Иринагей"){
+   
+    if(message.content === "#Иринагей"){
       setInterval(function() {
           let answers = ["иринагей", "ирина тупая", "ирина сосет", "ирина фрик"]; 
           let rand = Math.floor(Math.random()*answers.length); 
         message.channel.send(answers[rand]);
            }, 10000) ;
-         };    
+                    };    
      setTimeout(function() {
         clearInterval(timerid);
         }, 50000);
-    });
+              
+if (message.content === 'я не тупая') {
+    	message.reply('нет ты тупая');
+   	              };
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
